2aa616602632328aa8c41c3e7feac24a419261a5
D'Angelo Minnis <dangelominnis@gmail.com>
2020-11-18 14:45:29 -0800

    testing SAMESITE on .js

--- main.js
+++ main.js
@@ -16,3 +16,8 @@ const toggle = document.getElementById("nav__toggle");
 toggle.addEventListener("click", () => {
   nav.classList.toggle("active");
 });
+
+// Set a same-site cookie for first-party contexts
+document.cookie = 'cookie1=value1; SameSite=Lax';
+// Set a cross-site cookie for third-party contexts
+document.cookie = 'cookie2=value2; SameSite=None; Secure';
